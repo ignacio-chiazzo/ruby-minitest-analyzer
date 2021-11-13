a6765340e8279e45f3fb330d5c65051deb4eaa87
Ignacio Chiazzo Cardarello <ignaciochiazzo@gmail.com>
2021-11-13 16:36:22 -0500

    Modularize

--- minitest.rb
+++ minitest.rb
@@ -1,99 +1,183 @@
 require "minitest/autorun"
-require "pry"
-require 'pry-byebug'
 require 'table_print'
 
-require_relative 'tests/models/product'
-require_relative 'tests/tests_classes/product_grand_parent_test'
-require_relative 'tests/tests_classes/product_parent_test'
+class SetupInitializerAbstract
+  def setup
+    print_tests_stats do
+      require_all_test_files
+    end
+  end
 
-class SingleClassDuplicatedTestsSummary
+  protected
+
+  def test_classes
+    # returns all the tests classes in an array. E.g:
+    # Dir["test/**/*.rb"]
+    raise NotImplementedError, "test_classes must be implemented"
+  end
+
+  # returns all classes required to run the tests. E.g.
+  # ["../../../test/test_helper"]
+  def required_classes
+    raise NotImplementedError, "required_classes must be implemented"
+  end
+
+  private
+
+  def require_all_test_files
+    required_classes.each { |f| require_relative(f) }
+
+    test_classes.each do |f|
+      next if f == current_location_source.first
+      require_relative(f)
+    end
+  end
+
+  def print_tests_stats(&block)
+    puts "Requiring files..."
+    minitest_classes = yield
+    print_analyzer_stats(minitest_classes)
+    puts "All files required!"
+  end
+
+  def print_analyzer_stats(minitest_classes)
+    puts "Total of #{minitest_classes.count} test classes to analyze. \n"
+  end
+
+  def current_location_source
+    # Returns the current file location
+    self.class.instance_method(:setup).source_location
+  end
+end
+
+class SetupInitializer < SetupInitializerAbstract
+  protected
+
+  def test_classes
+    test_files_locations.flat_map do |test_file|
+      Dir[test_file]
+    end
+  end
+
+  def required_classes
+    ["../../../test/test_helper"]
+  end
+
+  private
+
+  def test_files_locations
+    [
+      Rails.root.join("components/billing/test/**/*.rb")
+    ]
+  end
+end
+
+class SingleTestClassSummary
   def initialize(klass:, runnable_tests_count: , extra_executions_run: , extra_tests_executions_count:, subclasses:)
     @klass = klass
-    @runnable_tests_count = runnable_tests_count 
+    @runnable_tests_count = runnable_tests_count
     @extra_executions_run = extra_executions_run
     @extra_tests_executions_count = extra_tests_executions_count
     @subclasses = subclasses
   end
 
-  attr_accessor :klass, :runnable_tests_count, :extra_executions_run, :extra_tests_executions_count, :subclasses
+  attr_accessor :klass, :runnable_tests_count, :extra_executions_run, :extra_tests_executions_count
+  attr_reader :subclasses
 
   def add_subclass(subc)
     @subclasses << subc
   end
 end
 
+class TestSummaryPresenter
+  def initialize(duplicated_suites)
+    @duplicated_suites = duplicated_suites
+    @total_extra_classes = @duplicated_suites.count
+    @total_extra_tests = total_extra_tests_across_suites
+  end
+
+  def present
+    puts "\n\n"
+    puts "*" * 50
+    puts "Total duplicated tests that can be removed: #{@total_extra_tests}"
+    puts "Total classes with duplicated tests: #{@total_extra_classes} "
+    puts "-" * 50
+    puts "\nClasses that run the tests multiple times: \n\n"
+
+    list = @duplicated_suites.map { |klass_name, summary| summary }
+    tp.set :max_width, 40
+    tp(
+      list,
+      { Class: lambda { |s| s.klass.name.demodulize } },
+      "extra_executions_run",
+      "runnable_tests_count",
+      "extra_tests_executions_count",
+      "klass"
+    )
+    puts "*" * 50
+    puts "\n\n"
+  end
+
+  private
+
+  def total_extra_tests_across_suites
+    @duplicated_suites.sum { |klass, info| info.extra_tests_executions_count  }
+  end
+end
+
 class MinitestsAnalyzer < Minitest::Test
   class << self
     def analyze
-      minitests_classes = load_minitests_classes
+      minitest_classes = load_minitest_classes.to_set
       duplicated_suites = {}
 
-      minitests_classes.each_with_index do |a|
-        minitests_classes.each_with_index do |b|
-          next if a == b 
-          
-          # b < a.  a -> parent, b -> child
-          if b.ancestors.include?(a)
-            parent_tests_runnable =  tests_methods(a)
-            if duplicated_suites[a.name]
-              info = duplicated_suites[a.name]
-              info.extra_executions_run += 1
-              info.extra_tests_executions_count += info.runnable_tests_count
-              info.add_subclass(b)
-              info
-            else
-              tests_count = parent_tests_runnable.count
-              duplicated_suites_info =  SingleClassDuplicatedTestsSummary.new(
-                klass: a,
-                runnable_tests_count: tests_count, 
-                extra_executions_run: 1, 
-                extra_tests_executions_count: tests_count, 
-                subclasses: [b]
-              )
+      minitest_classes.each_with_index do |child_a|
+        child_a.ancestors.each do |ancestor_a|
+          # ancestor_a
+          #   child_a
 
-              duplicated_suites[a.name] = duplicated_suites_info
-            end
+          next if child_a == ancestor_a
+
+          # skip if the ancestor is not a minitest class
+          next unless minitest_classes.include?(ancestor_a)
+
+          ancestor_a_runnable_tests = tests_methods(ancestor_a)
+          child_a_runnable_tests = tests_methods(child_a)
+
+          # If the parent or child don't have any runnable test then they don't have duplicated.
+          next unless ancestor_a_runnable_tests.count > 0
+          next unless child_a_runnable_tests.count > 0
+
+          # ancestor_a and child_a have also runnable tests, there are duplicated tests.
+          if duplicated_suites[ancestor_a.name]
+            info = duplicated_suites[ancestor_a.name]
+            info.extra_executions_run += 1
+            info.extra_tests_executions_count += info.runnable_tests_count
+            info.add_subclass(child_a)
+            info
+          else
+            tests_count = ancestor_a_runnable_tests.count
+            duplicated_suites_info =  SingleTestClassSummary.new(
+              klass: ancestor_a,
+              runnable_tests_count: tests_count,
+              extra_executions_run: 1,
+              extra_tests_executions_count: tests_count,
+              subclasses: [child_a]
+            )
+
+            duplicated_suites[ancestor_a.name] = duplicated_suites_info
           end
         end
       end
 
-      analyze_data(duplicated_suites)
+      duplicated_suites
     end
 
     private
 
-    def load_minitests_classes
-      @minitests_classes ||= Minitest::Runnable.runnables.reject { |s| s.runnable_methods.empty? }
+    def load_minitest_classes
+      @minitest_classes ||= Minitest::Runnable.runnables.reject { |s| s.runnable_methods.empty? }
     end
-
-    def prints_message(duplicated_suites, total_extra_classes, total_extra_tests)
-      puts "\n\n"
-      puts "*" * 50
-      puts "Total duplicated tests that can be removed: #{total_extra_tests}"
-      puts "Total classes with duplicated tests: #{total_extra_classes} "
-      puts "-" * 50
-      puts "\nClasses that run the tests multiple times: \n"
-
-      list = duplicated_suites.map { |klass_name, summary| summary }
-      tp list, "klass", "extra_executions_run", "runnable_tests_count", "extra_tests_executions_count"
-      puts "*" * 50
-      puts "\n\n"
-    end
-
-    def analyze_data(duplicated_suites)
-      total_extra_tests  = total_extra_tests_across_suites(duplicated_suites)
-      total_extra_classes = duplicated_suites.count
-      prints_message(duplicated_suites, total_extra_classes, total_extra_tests)
-    end
-
-    def total_extra_tests_across_suites(duplicated_suites)
-      duplicated_suites.sum { |klass, info| info.extra_tests_executions_count  }
-    end
-
-    # def descendants(klass)
-    #   ObjectSpace.each_object(Class).select { |k| k < klass }
-    # end
 
     def tests_methods(klass)
       re = /test_/
@@ -102,4 +186,12 @@ class MinitestsAnalyzer < Minitest::Test
   end
 end
 
-MinitestsAnalyzer.analyze
+# Load all the tests
+SetupInitializer.new.setup
+
+puts "Running"
+duplicated_suites_data = MinitestsAnalyzer.analyze
+presenter = TestSummaryPresenter.new(duplicated_suites_data)
+presenter.present()
+Process.exit!
+puts "Finish"
